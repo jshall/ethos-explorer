5177c8b19bbbd039a4c99d555e83b08b06931786
Josh Hall <josh.hall@indwes.edu>
2021-01-04 17:49:14 -0500

    partial implementation of advanced search

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -3,7 +3,7 @@ import { Subject } from 'rxjs';
 import { Domain, Entity, EthosData, Version } from 'src/ethos';
 
 let currentToken: SearchToken
-const resultSource = new Subject<SearchResult>()
+const resultSource = new Subject<SearchState>()
 
 @Injectable({
   providedIn: 'root'
@@ -16,100 +16,139 @@ export class SearchService {
 
   constructor() { }
 
-  search(query: string): void {
-    if (!query) {
-      this.resetSource.next()
-      return
-    }
+  search(query: query): void {
+    if (!query)
+      return this.reset()
     let token = new SearchToken(query)
     currentToken = token
     setTimeout(() => {
-      EthosData.domains.map(domain => this.searchDomain(domain, token))
+      EthosData.domains.map(domain => this.searchDomain(token.newState(domain)))
     }, 0)
   }
 
-  private childSearches(token: SearchToken, item: object, children: Promise<SearchResult>[]): Promise<SearchResult> {
+  reset() { this.resetSource.next() }
+
+  private childSearches(state: SearchState, children: Promise<SearchState>[]): Promise<SearchState> {
     let settled = 0
     let found = false
     return new Promise(resolve =>
       children.forEach(child => child
         .then(result => {
-          if (result.success) {
+          if (state.merge(result)) {
             found = true
-            resolve(new SearchResult(token, item))
+            resolve(state)
           }
         })
         .finally(() => {
           if (!found && ++settled === children.length)
-            resolve(new SearchResult(token, item, new Error('Not Found')))
+            resolve(state)
         })
       )
     )
   }
 
-  async searchDomain(domain: Domain, token: SearchToken): Promise<SearchResult> {
-    let children: Promise<SearchResult>[] = []
-    domain.subdomains?.forEach(domain => children.push(this.searchDomain(domain, token)))
-    domain.entities?.forEach(entity => children.push(this.searchEntity(entity, token)))
+  async searchDomain(state: SearchState): Promise<SearchState> {
+    if (!(state.item instanceof Domain))
+      throw new Error('State should represent a Domain')
+    let domain = state.item
+    let children: Promise<SearchState>[] = []
+    domain.subdomains?.forEach(domain => children.push(this.searchDomain(state.token.newState(domain))))
+    domain.entities?.forEach(entity => children.push(this.searchEntity(state.token.newState(entity))))
 
-    if (domain.name.toLowerCase().match(token.query.toLowerCase()))
-      return new SearchResult(token, domain)
+    if (state.test('title', domain.name))
+      return state
 
-    return await this.childSearches(token, domain, children)
+    return await this.childSearches(state, children)
   }
 
-  async searchEntity(entity: Entity, token: SearchToken): Promise<SearchResult> {
+  async searchEntity(state: SearchState): Promise<SearchState> {
+    if (!(state.item instanceof Entity))
+      throw new Error('State should represent a Entity')
+    let entity = state.item
     if (!entity.versions)
       await entity.getVersions()
-    let children = entity.versions!.map(version => this.searchVersion(version, token))
+    let children = entity.versions!.map(version => this.searchVersion(state.token.newState(version)))
 
-    if (entity.name.toLowerCase().match(token.query.toLowerCase()))
-      return new SearchResult(token, entity)
-    if (entity.resource.toLowerCase().match(token.query.toLowerCase()))
-      return new SearchResult(token, entity)
+    if (state.test('title', entity.name))
+      return state
+    if (state.test('resource', entity.resource))
+      return state
 
-    return await this.childSearches(token, entity, children)
+    return await this.childSearches(state, children)
   }
 
-  async searchVersion(version: Version, token: SearchToken): Promise<SearchResult> {
-    return new SearchResult(token, version, new Error('Method not implemented.'))
+  async searchVersion(state: SearchState): Promise<SearchState> {
+    if (!(state.item instanceof Version))
+      throw new Error('State should represent a Version')
+    let version = state.item
+    if (state.test('version', version.name))
+      return state
+    if (Object.keys(version.systems).some(sys => state.test('system', sys)))
+      return state
+
+    return state
   }
 }
 
-class AggregateError extends Error {
-  errors: any[]
+type query = string | [string, ...query[]]
+type state = boolean | [string, ...state[]]
 
-  constructor(message?: string, ...errors: Error[]) {
-    super(message)
-    this.errors = errors
+class SearchState {
+  private readonly state: state
+
+  readonly token: SearchToken
+  readonly item: object
+  get success() { return this.reduce(this.state) }
+  get active() { return this.token === currentToken }
+
+  constructor(token: SearchToken, item: object) {
+    this.token = token
+    this.item = item
+    this.state = this.newState(token.query)
+  }
+
+  private newState(query: query): state {
+    if (typeof query === 'string')
+      return false
+    let [key, ...words] = query
+    let state: [string, ...state[]] = [key]
+    words.forEach(word => {
+      if (typeof word === 'string')
+        state.push(false)
+      else
+        state.push(this.newState(word))
+    })
+    return state
+  }
+
+  private reduce(state: state): boolean {
+    if (typeof state === 'boolean')
+      return state
+    let [op, ...states] = state
+    switch (op) {
+      case '|': return states.reduce<boolean>((p, c) => p || this.reduce(c), false)
+      case '&': return states.reduce<boolean>((p, c) => p && this.reduce(c), true)
+      default: return this.reduce(states[0])
+    }
+  }
+
+  test(id: string, value: string): boolean {
+    // TODO: cycle query and update state
+    return this.success
+  }
+
+  merge(result: SearchState) {
+    // TODO: merge result state into this one
+    return this.success
   }
 }
 
 export class SearchToken {
-  query: string
+  readonly query: query
 
-  constructor(query: string) {
+  constructor(query: query) {
     this.query = query
   }
-}
 
-export class SearchResult {
-  token: SearchToken
-  item: object
-  success: boolean
-  error?: Error
-
-  get active() {
-    return this.token === currentToken
-  }
-
-  constructor(token: SearchToken, item: object, error?: Error) {
-    this.token = token
-    this.item = item
-    this.success = !error
-    if (error)
-      this.error = error
-
-    resultSource.next(this)
-  }
+  newState(item: object) { return new SearchState(this, item) }
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Grammar, Parser } from 'nearley';
 import { SearchService } from '../search.service';
+import grammar from './search.grammar';
 
 @Component({
   selector: 'app-search',
@@ -12,7 +14,25 @@ export class SearchComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  search(value: string) {
-    this.service.search(value)
+  search(event: any) {
+    let target = event.target as HTMLInputElement
+    let value = target.value as string
+    if (!value)
+      return this.reset()
+    let parser = new Parser(Grammar.fromCompiled(grammar))
+    try {
+      parser.feed(value)
+      if (parser.results.length != 1)
+        throw new Error('Ambiguous or incomplete result.')
+      target.style.backgroundColor = ''
+      this.service.search(parser.results[0])      
+    } catch (error) {
+      console.warn(error)
+      target.style.backgroundColor = 'salmon'
+    }
+  }
+
+  reset() {
+    this.service.reset()
   }
 }
